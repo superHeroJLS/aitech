509bfbbcdc92b31a7261f7851f5ece600a855694
jiangls <1257892551@qq.com>
2026-05-29 01:18:22 +0800

    add icp

---
+++ docs/.vitepress/theme/Layout.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import DefaultTheme from 'vitepress/theme'
+
+const { Layout } = DefaultTheme
+</script>
+
+<template>
+  <Layout>
+    <template #layout-bottom>
+      <div class="beian-footer">
+        <a
+          href="https://beian.miit.gov.cn/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >赣ICP备2026011221号-1</a>
+      </div>
+    </template>
+  </Layout>
+</template>
