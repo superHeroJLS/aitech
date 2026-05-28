0fff71c580e247e23f291283ed745efdc96c5181
jiangls <1257892551@qq.com>
2026-05-29 01:06:42 +0800

    add icp 备案

---
+++ docs/.vitepress/theme/Layout.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import DefaultTheme from 'vitepress/theme'
+</script>
+
+<template>
+  <DefaultTheme.Layout>
+    <template #layout-bottom>
+      <div class="icp-beian">
+        <a
+          href="https://beian.miit.gov.cn/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >赣ICP备2026011221号-1</a>
+      </div>
+    </template>
+  </DefaultTheme.Layout>
+</template>
